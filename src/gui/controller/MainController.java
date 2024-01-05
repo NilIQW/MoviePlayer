f8e6125ab64ebd97ab228c9f95d9913b9c92d964
Nil <nilyos01@easv365.dk>
2024-01-05 20:07:46 +0100

    implemented add button and edited gui design

--- src/gui/controller/MainController.java
+++ src/gui/controller/MainController.java
@@ -1,8 +1,21 @@
 package gui.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
-    public void addMovieButton(ActionEvent actionEvent) {
+    public void addMovieButton(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/view/Movie.fxml"));
+        Parent root = loader.load();
+        MovieController movieController = loader.getController();
+        Stage stage = new Stage();
+        stage.setTitle("Media Player");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
