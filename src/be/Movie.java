58ad21af88075d7519e23efad01bd275cd932aeb
SirRivol <71494331+Rivol0081@users.noreply.github.com>
2024-01-07 16:40:51 +0100

    be

--- src/be/Movie.java
+++ src/be/Movie.java
@@ -1,4 +1,59 @@
 package be;
 
+import java.time.LocalDate;
+
 public class Movie {
+    private int id;
+    private String title;
+    private Double rating;
+    private String path;
+    private LocalDate date;
+
+    public Movie(int id, String title, Double rating, String path, LocalDate date){
+        this.id = id;
+        this.title = title;
+        this.rating = rating;
+        this.path = path;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
