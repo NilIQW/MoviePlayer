49c923b5b4938525d101f3105f91997035f9cc64
Andrii Savchenko <109040480+andriiVIt@users.noreply.github.com>
2024-01-09 15:49:40 +0100

    ..

--- src/gui/controller/NewCategoryController.java
+++ src/gui/controller/NewCategoryController.java
@@ -1,18 +1,42 @@
 package gui.controller;
-
+import be.Category;
+import gui.Model;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class NewCategoryController {
-    public void addCat(ActionEvent event) {
-    }
+    @FXML
+    private Button closeButton;
+    @FXML
+    private TextField textField;
 
+    private Model model;
+
+    public void addCat(ActionEvent event) {
+        //SaveCategory( event);
+    }
+    public void setModel(Model model) {
+        this.model = model;
+
+    }
     public void SaveCategory(ActionEvent event) {
+        Model model = Model.getInstance();
+       model.createCategory(textField.getText());
+
+        textField.clear();
+        closeWindow(event);
     }
 
     public void closeWindow(ActionEvent event) {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
     }
 
 
-    public void setCategories() {
+   public void setCategories() {this.model = model;
+
     }
 }
