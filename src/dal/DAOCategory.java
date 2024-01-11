133afa90a20764f3135a0098209f49d37f44e1ce
Nil <nilyos01@easv365.dk>
2024-01-11 14:59:46 +0100

    saveBtn

--- src/dal/DAOCategory.java
+++ src/dal/DAOCategory.java
@@ -1,4 +1,27 @@
 package dal;
 
-public class DAOCategory {
+import be.Category;
+import dal.ConnectionManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CategoryDao {
+    private ConnectionManager connectionManager;
+    public CategoryDao(ConnectionManager connectionManager) {
+        this.connectionManager = connectionManager;
+    }
+
+    public void saveCategory(Category category) {
+        try (Connection connection = connectionManager.getConnection()) {
+            String sql = "INSERT INTO category (name) VALUES (?)";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, category.getName());
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
