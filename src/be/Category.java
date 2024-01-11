5c0ac0f16fe5c76e86ca3bd2257c40d5012cbbc8
Meile5 <meile.kapisaite5@gmail.com>
2024-01-11 15:01:51 +0100

    .

--- src/be/Category.java
+++ src/be/Category.java
@@ -3,13 +3,25 @@ package be;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Category {
+    private ArrayList<Movie> allMovies;
     private int id;
 
     private String name;
 
     public Category(String name){
+        this.allMovies = new ArrayList<Movie>();
         this.name = name;
+    }
+
+    public void setAllMovies(ArrayList<Movie> songs) {
+        this.allMovies = songs;
+    }
+    public List<Movie> getAllMovies(){
+        return allMovies;
     }
 
 
